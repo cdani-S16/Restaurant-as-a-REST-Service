b3241765d50e137a2ac37d376fcf4b007e3f5fac
Chris <cdani@hawk.iit.edu>
2016-02-24 11:11:25 +0000

    added test class, yet to complete it, also added items in menuitem class

---
+++ src/delectable/EntityTesting.java
@@ -0,0 +1,20 @@
+package delectable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntityTesting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+		MenuItem testMenuItem = new MenuItem();
+		
+		
+		
+		
+	}
+
+}
