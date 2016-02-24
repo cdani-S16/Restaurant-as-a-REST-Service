496e9b2bdf91a3f71af4f47a062b2345d8f100bf
Chris <cdani@hawk.iit.edu>
2016-02-24 21:45:07 +0530

    Added menu testing , tested for menuitem creating an item

---
+++ src/delectable/MenuTesting.java
@@ -0,0 +1,26 @@
+package delectable;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MenuTesting {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		MenuItem testMenuItem = new MenuItem("John", 45, 2, 2);
+		Menu testMenu = new Menu();
+		testMenu.AddItem(testMenuItem);
+	}
+
+}
